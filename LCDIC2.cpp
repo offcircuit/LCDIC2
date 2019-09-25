200211f831e0b4e595a62258e6bc0ef066268019
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-09-26 00:14:46 +0100

    Update LCDIC2.cpp

--- LCDIC2.cpp
+++ LCDIC2.cpp
@@ -24,12 +24,12 @@ bool LCDIC2::end() {
 }
 
 void LCDIC2::bounds(uint8_t &x, uint8_t &y) {
-  uint8_t length = 0x4F;
   if (_height == 4)
-    if (_width == 16) length = 0x0F + (y / 2) * 0x08;
-    else length = _width - 1;
-  else if (_height == 2) length = 0x27;
-  x = x < length ? x : length;
+    if (_width == 16) _width = 0x0F + (y / 2) * 0x08;
+    else _width = _width - 1;
+  else if (_height == 2) _width = 0x27;
+  else _width = 0x4F;
+  x = x < _width ? x : _width;
   y = y < uint8_t(_height - 1) ? y : uint8_t(_height - 1);
 }
 
