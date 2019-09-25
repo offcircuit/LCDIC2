d9da4946896a7296e439a5f9d5934854898eef6f
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-09-26 00:30:49 +0100

    Update hello_world.ino

--- examples/hello_world/hello_world.ino
+++ examples/hello_world/hello_world.ino
@@ -2,14 +2,17 @@
 
 LCDIC2 lcd(0x27, 16, 2);
 
-
 void setup() {
   if (lcd.begin()) lcd.print("Hello, World!");
 }
 
 void loop() {
-  for (uint8_t i = 0; i < 16; i++){
-    lcd.setCursor(i, 0);
-    delay(150);
+  for (uint8_t i = 0; i < 15; i++) {
+    lcd.setCursor(i, 1);
+    delay(250);
+  }
+  for (uint8_t i = 15; i > 0; i--) {
+    lcd.setCursor(i, 1);
+    delay(250);
   }
 }
