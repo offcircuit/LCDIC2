77b7edcabdc56a7a5fc1641486e243523f4c1810
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-09-26 22:40:38 +0100

    Update LCDIC2.cpp

--- LCDIC2.cpp
+++ LCDIC2.cpp
@@ -64,15 +64,15 @@ uint8_t LCDIC2::flag() {
 }
 
 void LCDIC2::getCursor(uint8_t &x, uint8_t &y) {
-      x = request(0b10);
-      y = 0;
-      if (_height == 2) y = x > start(0);
-      else if (_height == 4) {
-        if (x > start(3)) y = 3;
-        else if (x > start(1)) y = 1;
-        else if (x > start(2)) y = 2;
-      }
-      x = x - start(y);
+  x = request(0b10);
+  y = 0;
+  if (_height == 2) y = x > start(0);
+  else if (_height == 4) {
+    if (x > start(3)) y = 3;
+    else if (x > start(1)) y = 1;
+    else if (x > start(2)) y = 2;
+  }
+  x = x - start(y);
 }
 
 bool LCDIC2::home() {
