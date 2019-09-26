75fbf29d566c92889cd1bc153c92344c18c0d2ac
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-09-26 20:45:07 +0100

    Update LCDIC2.h

--- LCDIC2.h
+++ LCDIC2.h
@@ -26,11 +26,13 @@ class LCDIC2 {
   private:
     bool _backlight = true, _blink = false, _cursor = true, _display = true, _font = 0, _gain = true, _shift = false;
     uint8_t _address, _height = 0, _width = 0;
-    //void bounds(uint8_t &x, uint8_t &y);
+    void bounds(uint8_t &x, uint8_t &y);
     bool busy();
     uint8_t flag();
+    uint8_t length(uint8_t y);
     uint8_t request(uint8_t rs);
     bool send(uint8_t data, uint16_t us = 0);
+    uint8_t start(uint8_t y);
     void wait(uint16_t us);
     bool write(uint8_t data, uint8_t rs = 0);
     bool writeHigh(uint8_t data, uint8_t rs = 0);
@@ -46,7 +48,7 @@ class LCDIC2 {
     bool clear();
     bool cursorLeft();
     bool cursorRight();
-    // void getCursor(uint8_t &x, uint8_t &y);
+    void getCursor(uint8_t &x, uint8_t &y);
     bool home();
     bool leftToRight();
     bool moveLeft();
@@ -57,41 +59,13 @@ class LCDIC2 {
     bool setBacklight(bool state);
     bool setBlink(bool state);
     bool setCursor(bool state);
-    //bool setCursor(uint8_t x, uint8_t y);
+    bool setCursor(uint8_t x, uint8_t y);
     bool setDisplay(bool state);
     bool setFont(bool font);
     bool setGlyph(uint8_t glyph, uint8_t data[]);
     bool setLines(uint8_t height);
     bool setShift(bool state);
     bool sift(uint8_t glyph, uint8_t *&data);
-
-
-    uint8_t start(uint8_t y) {
-      return ((y % 2) << 6) + ((y / 2) * _width);
-    }
-
-    uint8_t length(uint8_t y) {
-      return (_height - 1) ? (((0b1001 >> (_height > 2)) - (_width / _height == 4)) << 2 | 0b11) + (((_width / _height == 4) & (y / 2)) << 3) : 0b1001111;
-    }
-
-    void bounds(uint8_t &x, uint8_t &y) {
-      y = min(y, uint8_t(_height - 1));
-      x = min(x, uint8_t(length(y)));
-    }
-
-
-    void getCursor(uint8_t &x, uint8_t &y) {
-      x = request(0b10);
-      y = 0;
-      while ((y < _height - 1) && (x > start((0b10011100 >> 6 - (2 * y)) & 0b11))) y++;
-      x = x - start((0b10011100 >> 6 - (2 * (y - 1))) & 0b11);
-    }
-
-    bool setCursor(uint8_t x, uint8_t y) {
-      bounds(x, y);
-      return write(LCDIC2_DDRAM | start(y) + x);
-    }
 };
-
 
 #endif
